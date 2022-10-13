ddcc2b562b5482b9a64737213d90f94ecd580dc2
ahmetgozel <a.gozel.35@gmail.com>
2022-10-13 22:54:57 +0300

    Exception Handler mechanism added

--- src/main/java/com/library/exception/message/ErrorMessage.java
+++ src/main/java/com/library/exception/message/ErrorMessage.java
@@ -3,5 +3,9 @@ package com.library.exception.message;
 public class ErrorMessage {
 
     public final static String LOAN_TIME_INCORRECT_MESSAGE ="Loan pick up time or drop off time not correct";
+    public final static String USER_NOT_FOUND_MESSAGE="User with email %s not found";
+    public final static String EMAIL_ALREADY_EXIST_MESSAGE="Email already exist: %s";
+    public final static String ROLE_NOT_FOUND_MESSAGE="Role with name %s not found";
+
 
 }
