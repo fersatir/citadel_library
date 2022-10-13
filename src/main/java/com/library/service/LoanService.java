5817805cce61295c1da351aa729b23e8c82a9c49
okabdullahss <okabdullahss@gmail.com>
2022-10-13 10:56:24 +0300

    loan transactions were added

--- src/main/java/com/library/service/LoanService.java
+++ src/main/java/com/library/service/LoanService.java
@@ -1,37 +1,57 @@
 package com.library.service;
 
-import com.library.DTO.requests.LoanRequest;
+import com.library.DTO.LoanDTO;
+import com.library.DTO.mapper.LoanMapper;
+import com.library.domain.Book;
 import com.library.domain.Loan;
+import com.library.domain.Role;
+import com.library.domain.User;
+import com.library.domain.enums.RoleType;
 import com.library.exception.BadRequestException;
 import com.library.exception.message.ErrorMessage;
+import com.library.repository.BookRepository;
 import com.library.repository.LoanRepository;
+import com.library.repository.RoleRepository;
+import com.library.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
 public class LoanService {
 
     LoanRepository loanRepository;
+    LoanMapper loanMapper;
 
-    public void createLoan(LoanRequest loanRequest){
+    BookRepository bookRepository;
+    UserRepository userRepository;
 
-        checkLoanTimeIsCorrect(loanRequest.getLoanDate(),loanRequest.getReturnDate());
+    public LoanDTO createLoan(LoanDTO loanDTO){
+
+        checkLoanTimeIsCorrect(loanDTO.getLoanDate(), loanDTO.getReturnDate());
 
         Loan loan = new Loan();
 
-        loan.setLoanDate(loanRequest.getLoanDate());
-        loan.setBook(loanRequest.getBook());
-        loan.setExpireDate(loanRequest.getExpireDate());
-        loan.setNotes(loan.getNotes());
-        loan.setReturnDate(loanRequest.getReturnDate());
-        loan.setUser(loanRequest.getUser());
+        Book book = bookRepository.findById(loanDTO.getBookId()).orElse(null);
+        loan.setBook(book);
+
+
+        User user = userRepository.findById(loanDTO.getUserId()).orElse(null);
+        loan.setUser(user);
+
+        loan = loanMapper.loanDTOToLoan(loanDTO);
 
         loanRepository.save(loan);
+        loanDTO.setId(loan.getId());
+        return loanDTO;
 
     }
+
 
     private void checkLoanTimeIsCorrect(LocalDateTime loanDate, LocalDateTime returnDate) {
         LocalDateTime now = LocalDateTime.now();
