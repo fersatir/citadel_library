7a9f0512c1b83fdf41bebecdebc91c25b1ce0581
ahmetgozel <a.gozel.35@gmail.com>
2022-10-14 11:57:37 +0300

    jwt security added

--- src/main/java/com/library/controller/UserController.java
+++ src/main/java/com/library/controller/UserController.java
@@ -6,6 +6,7 @@ import com.library.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,7 @@ public class UserController {
     private UserService userService;
 
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/add")
     public ResponseEntity<UserCreateDTO> createUser(@Valid @RequestBody UserCreateDTO userCreateDTO){
 
