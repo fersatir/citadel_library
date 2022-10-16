dcc7f0898d65fec9da3d167e3992ac7061c7f34a
Yasin Türk <punisher10punisher@gmail.com>
2022-10-16 13:51:30 +0300

    publisher end-points

--- src/main/java/com/library/controller/PublisherController.java
+++ src/main/java/com/library/controller/PublisherController.java
@@ -3,12 +3,14 @@ package com.library.controller;
 import com.library.domain.Publisher;
 import com.library.service.PublisherService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
@@ -32,5 +34,34 @@ public class PublisherController {
         response.put("publisher", publisher);
 
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/publishers")
+    public ResponseEntity<Page<Publisher>> getAllPublishers(@RequestParam("page") int page,
+                                                            @RequestParam("size") int size,
+                                                            @RequestParam("sort") String prop,
+                                                            @RequestParam("type") Direction direction){
+        Pageable pageable = PageRequest.of(page, size, Sort.by(direction,prop));
+        Page<Publisher> publisherPage = publisherService.getAllPublishersWithPage(pageable);
+        return new ResponseEntity<>(publisherPage, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Publisher> getPublisherById(@PathVariable Long id){
+        Publisher publisher = publisherService.getPublisherById(id);
+        return ResponseEntity.ok(publisher);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Publisher> updatePublisherById(@PathVariable Long id, @Valid @RequestBody Publisher newPublisher){
+        Publisher publisher = publisherService.updatePublisherById(id, newPublisher);
+        return ResponseEntity.ok(publisher);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Publisher> deletePublisherById(@PathVariable Long id){
+        Publisher publisher = publisherService.deletePublisherById(id);
+        return ResponseEntity.ok(publisher);
     }
 }
--- src/main/java/com/library/service/PublisherService.java
+++ src/main/java/com/library/service/PublisherService.java
@@ -4,6 +4,8 @@ import com.library.domain.Author;
 import com.library.domain.Publisher;
 import com.library.repository.PublisherRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
@@ -16,4 +18,27 @@ public class PublisherService {
         publisherRepository.save(publisher);
     }
 
+    public Page<Publisher> getAllPublishersWithPage(Pageable pageable) {
+      return  publisherRepository.findAllPublishersWithPage(pageable);
+    }
+
+    public Publisher getPublisherById(Long id) {
+        return publisherRepository.findById(id).orElse(null);
+        //TODO -> Exception
+    }
+
+    public Publisher updatePublisherById(Long id, Publisher newPublisher) {
+        Publisher foundPublisher = getPublisherById(id);
+        foundPublisher.setName(newPublisher.getName());
+        foundPublisher.setBuiltIn(newPublisher.getBuiltIn());
+
+        publisherRepository.save(foundPublisher);
+        return foundPublisher;
+    }
+
+    public Publisher deletePublisherById(Long id) {
+        Publisher foundPublisher = getPublisherById(id);
+        publisherRepository.deleteById(id);
+        return foundPublisher;
+    }
 }
