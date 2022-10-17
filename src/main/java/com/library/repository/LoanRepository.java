484137dec679fce34a4448f4eb5f4c20baa6e9a8
ahmetgozel <a.gozel.35@gmail.com>
2022-10-17 11:23:09 +0300

    ...

--- src/main/java/com/library/repository/LoanRepository.java
+++ src/main/java/com/library/repository/LoanRepository.java
@@ -5,17 +5,23 @@ import com.library.domain.Loan;
 import com.library.domain.User;
 import com.library.dto.response.LoanResponse;
 import com.library.dto.response.LoanResponseBook;
-import com.library.dto.response.UserLoansResponse;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
+import com.library.dto.response.UserLoansResponse;
 import java.util.List;
 
 @Repository
 public interface LoanRepository extends JpaRepository<Loan,Long> {
+
+
+
+
+
+
 
     @Query("SELECT u FROM Loan u WHERE u.user.id = ?1")
     List<Loan> expireDate(Long id);
@@ -28,6 +34,12 @@ public interface LoanRepository extends JpaRepository<Loan,Long> {
 
   @Query("SELECT new com.library.dto.response.LoanResponseBook(u) FROM Loan u WHERE u.book.id = ?1")
   Page<LoanResponseBook> getSpecifiedBookLoan(Long id, Pageable pageable);
+
+
+
+
+
+
 
     @Query("SELECT new com.library.dto.response.UserLoansResponse(u) FROM Loan u WHERE u.user.id = ?1")
     Page<UserLoansResponse> getAuthUserLoans(Pageable pageable, Long id);
